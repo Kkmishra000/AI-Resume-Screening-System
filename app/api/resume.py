7a8c40b10594f710f11249ae5e42d531056c98f1
Krishna <stunningkk45@gmail.com>
2026-07-07 18:39:58 +0530

    Added resume upload endpoint

--- app/api/resume.py
+++ app/api/resume.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,UploadFile, File
 
 
 router = APIRouter()
@@ -6,4 +6,11 @@ router = APIRouter()
 @router.get("/")
 def home():
     return {"message":"welcome to AI resume screening System"}
+
+
+@router.post("/upload_resume")
+def upload_resume(file: UploadFile = File(...)):
+    # Process the uploaded resume file
+    # You can save the file, analyze it, or perform any other operations here
+    return {"filename": file.filename, "content_type": file.content_type}
 
